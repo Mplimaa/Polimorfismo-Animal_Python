dec0e297cbcb80599c036dde5133c9491079dc6a
Michele Lima <106484086+Mplimaa@users.noreply.github.com>
2025-09-09 10:41:36 -0300

    Update Animal.py

--- Animal.py
+++ Animal.py
@@ -1,3 +1,9 @@
+from abc import ABC, abstractmethod  # ← Import necessário
+#abc = Abstract Base Classes (Módulo interno do Python).
+#ABC → transforma a classe em abstrata (não pode ser instanciada).
+# abstractmethod → marca métodos que devem ser implementados pelas classes filhas.
+
+
 # Classe mãe (superclasse)
 class Animal:
     def __init__(self, nome, idade):
@@ -11,6 +17,13 @@ class Animal:
     def locomover(self):
         # Implementação genérica
         print("O animal está se locomovendo.")
+
+    @classmethod
+    @abstractmethod
+    def especie(cls):
+        pass
+
+
 
 
 
